1dfbf694fd4618be4679e052670a7686911e47a3
Ayush Nanda <ananda_be18@thapar.edu>
2020-11-10 10:43:52 +0530

    Extra features added

--- TextUtils/urls.py
+++ TextUtils/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('', views.index, name='index'),
-   path('analyse', views.analyze, name='analyse')
+   path('analyse', views.analyze, name='analyse'),
+   path('navigation', views.navigation, name='navigation'),
 ]
--- TextUtils/views.py
+++ TextUtils/views.py
@@ -11,8 +11,16 @@ def index(request):
 def analyze(request):
     # Get the text
     djtext = request.GET.get('text', 'default')
-    removepunc=request.GET.get('removepunc','off')
 
+    #Check checkbox values
+    removepunc=request.GET.get('removepunc','off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover=request.GET.get('newlineremover','off')
+    spaceremover=request.GET.get('spaceremover','off')
+    charactercount=request.GET.get('charactercount','off')
+
+
+    #Check which checkbox is on
     if removepunc == "on":
         punctuations = '''!()-[]{};:'",<>./?@#$%^&*_~'''
         analyzed = ""
@@ -21,6 +29,52 @@ def analyze(request):
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyse.html', params)
+    
+    elif(fullcaps=="on"):
+        analyzed=""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyse.html', params)
+
+    elif(newlineremover=="on"):
+        analyzed=""
+        for char in djtext:
+            if char !="/n":
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed new lines', 'analyzed_text': analyzed}
+        return render(request, 'analyse.html', params)
+    
+    elif(spaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+        # Analyze the text
+        return render(request, 'analyse.html', params)
+
+    elif(charactercount=='on'):
+        analyzed=0
+        for char in djtext:
+            if char != ' ': #not counting the space as a character
+                analyzed +=1
+
+
+        params = {'purpose':'charactercount', 'analyzed_text':analyzed}
+        return render(request, 'analyse.html', params)
+
+
+
 
     else:
         return HttpResponse('Error')
+
+def navigation(request):
+    s= '''<h2> Navigation Bar </h2>
+        <a href="https://www.youtube.com/">Youtube </a><br>
+        <a href="https://www.google.com/webhp?authuser=3">Google </a><br>
+        <a href="https://www.facebook.com/">Facebook</a> <br>'''
+    return HttpResponse(s)
