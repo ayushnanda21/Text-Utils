6ef2cf81967e0cc0380be55cf8eca1ae06625636
Ayush Nanda <ananda_be18@thapar.edu>
2020-11-12 21:51:25 +0530

    multi button func added

--- TextUtils/views.py
+++ TextUtils/views.py
@@ -28,36 +28,40 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyse.html', params)
+        djtext= analyzed     #for multi functioning
+        #return render(request, 'analyse.html', params)
     
-    elif(fullcaps=="on"):
+    if(fullcaps=="on"):
         analyzed=""
         for char in djtext:
             analyzed = analyzed + char.upper()
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
-        return render(request, 'analyse.html', params)
+        djtext= analyzed     #for multi functioning
+        #return render(request, 'analyse.html', params)
 
-    elif(newlineremover=="on"):
+    if(newlineremover=="on"):
         analyzed = ""
         for char in djtext:
             if char != "\n" and char!="\r":
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed new lines', 'analyzed_text': analyzed}
-        return render(request, 'analyse.html', params)
+        djtext= analyzed     #for multi functioning
+        #return render(request, 'analyse.html', params)
     
-    elif(spaceremover=="on"):
+    if(spaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not(djtext[index] == " " and djtext[index+1]==" "):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext= analyzed     #for multi functioning
 
         # Analyze the text
-        return render(request, 'analyse.html', params)
+        #return render(request, 'analyse.html', params)
 
-    elif(charactercount=='on'):
+    if(charactercount=='on'):
         analyzed=0
         for char in djtext:
             if char != ' ': #not counting the space as a character
@@ -65,13 +69,18 @@ def analyze(request):
 
 
         params = {'purpose':'charactercount', 'analyzed_text':analyzed}
-        return render(request, 'analyse.html', params)
+        djtext= analyzed     #for multi functioning
+        
+    if(removepunc != "on" and newlineremover!="on" and spaceremover!="on" and fullcaps!="on" and charactercount!="on"):
+        return HttpResponse("please select any operation and try again")
+        
+    return render(request, 'analyse.html', params)
 
 
 
 
-    else:
-        return HttpResponse('Error')
+    #else:
+        #return HttpResponse('Error')
 
 def navigation(request):
     s= '''<h2> Navigation Bar </h2>
